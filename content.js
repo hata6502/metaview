00227e81b0e528a85c36a00d1e414490aeb37455
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2021-12-18 23:07:55 +0900

    Update content.js

--- content.js
+++ content.js
@@ -1,13 +1,15 @@
-const description = document.querySelector('meta[name="description"]')?.content;
-const imageURL = document.querySelector('meta[property="og:image"]')?.content;
+setInterval(() => {
+  const description = document.querySelector('meta[name="description"]')?.content;
+  const imageURL = document.querySelector('meta[property="og:image"]')?.content;
 
-const title =
-  document.querySelector('meta[property="og:title"]')?.content ??
-  document.title;
+  const title =
+    document.querySelector('meta[property="og:title"]')?.content ??
+    document.title;
 
-chrome.runtime.sendMessage({
-  url: location.href,
-  description,
-  imageURL,
-  title,
-});
+  chrome.runtime.sendMessage({
+    url: location.href,
+    description,
+    imageURL,
+    title,
+  });
+}, 1000);
