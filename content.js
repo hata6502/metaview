2c860f7ae586f4b16c885f5195938c939f3ef6c3
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2022-01-01 03:50:35 +0900

    Article support

--- content.js
+++ content.js
@@ -1,3 +1,176 @@
+const sendPage = () => {
+  const structuredDataList = [
+    ...document.querySelectorAll('script[type="application/ld+json" i]'),
+  ].flatMap((jsonLDElement) => {
+    if (!(jsonLDElement instanceof HTMLScriptElement)) {
+      return [];
+    }
+
+    try {
+      const jsonLD = JSON.parse(jsonLDElement.innerText);
+      const jsonLDDocuments = Array.isArray(jsonLD) ? jsonLD : [jsonLD];
+
+      return jsonLDDocuments.filter(
+        (jsonLDDocument) =>
+          typeof jsonLDDocument === "object" &&
+          jsonLDDocument !== null &&
+          ["http://schema.org", "https://schema.org"].includes(
+            jsonLDDocument["@context"]
+          )
+      );
+    } catch (exception) {
+      console.error(exception);
+
+      return [];
+    }
+  });
+
+  const url = location.href;
+  const title = getTitle();
+
+  const articleStructuredDataImage = getArticleStructuredData({
+    structuredDataList,
+  })?.image[0];
+
+  const imageURL =
+    document.querySelector('meta[property="og:image" i]')?.content ||
+    (typeof articleStructuredDataImage === string &&
+      articleStructuredDataImage) ||
+    getSingleImageURL() ||
+    document.querySelector('link[rel="icon" i]')?.href;
+
+  chrome.runtime.sendMessage({
+    url,
+    description: `${[
+      title,
+      url,
+      [
+        getDateLine({ structuredDataList }),
+        getCreditLine({ structuredDataList }),
+      ]
+        .filter((line) => line)
+        .join("\n"),
+      getBreadcrumbs({ structuredDataList }),
+      getDescription({ structuredDataList }),
+      getHashTagLine(),
+    ]
+      .filter((line) => line)
+      .join("\n\n")}\n\n`,
+    imageURL,
+    metadata: [
+      ...[...document.querySelectorAll("meta[name]")].map(
+        (metaElement) => `${metaElement.name}:${metaElement.content}`
+      ),
+      ...[
+        ...document.querySelectorAll('script[type="application/ld+json" i]'),
+      ].map((scriptElement) => scriptElement.innerText),
+    ].join(" "),
+    title,
+  });
+};
+
+document.addEventListener("DOMContentLoaded", sendPage);
+addEventListener("load", sendPage);
+setInterval(sendPage, 1000);
+
+const getArticleStructuredData = ({ structuredDataList }) =>
+  structuredDataList.find((structuredData) =>
+    ["Article", "BlogPosting", "NewsArticle"].includes(structuredData["@type"])
+  );
+
+const getBreadcrumbs = ({ structuredDataList }) =>
+  structuredDataList
+    .flatMap((structuredData) => {
+      const { itemListElement } = structuredData;
+
+      if (
+        structuredData["@type"] !== "BreadcrumbList" ||
+        !Array.isArray(itemListElement)
+      ) {
+        return [];
+      }
+
+      return [
+        [...itemListElement]
+          .sort((a, b) => a.position - b.position)
+          .map((listItem) => `#${listItem.item.name}`)
+          .join(" > "),
+      ];
+    })
+    .join("\n");
+
+const getCreditLine = ({ structuredDataList }) => {
+  const articleStructuredData = getArticleStructuredData({
+    structuredDataList,
+  });
+
+  const credits = [
+    ...new Set(
+      [
+        articleStructuredData?.author?.name,
+        articleStructuredData?.publisher?.name,
+        ,
+        ...[
+          ...document.querySelectorAll(
+            'meta[name="author" i], meta[name="creator" i], meta[name="publisher" i]'
+          ),
+        ].flatMap((creditElement) =>
+          creditElement instanceof HTMLMetaElement
+            ? [creditElement.content]
+            : []
+        ),
+      ].filter((credit) => credit)
+    ),
+  ];
+
+  return credits.length >= 1 && `by ${credits.map(stringToHashTag).join(" ")}`;
+};
+
+const getDateLine = ({ structuredDataList }) => {
+  const articleStructuredData = getArticleStructuredData({
+    structuredDataList,
+  });
+
+  const dateString =
+    articleStructuredData?.dateModified ?? articleStructuredData?.datePublished;
+
+  return dateString && new Date(dateString).toLocaleString();
+};
+
+const getDescription = ({ structuredDataList }) => {
+  const descriptionElement = document.querySelector(
+    'meta[name="description" i]'
+  );
+
+  const ogDescriptionElement = document.querySelector(
+    'meta[property="og:description" i]'
+  );
+
+  return (
+    (ogDescriptionElement instanceof HTMLMetaElement &&
+      ogDescriptionElement.content) ||
+    (descriptionElement instanceof HTMLMetaElement &&
+      descriptionElement.content) ||
+    getArticleStructuredData({ structuredDataList })?.headline
+  );
+};
+
+const getHashTagLine = () => {
+  const keywordsElement = document.querySelector('meta[name="keywords" i]');
+
+  const joinedKeywords =
+    (keywordsElement instanceof HTMLMetaElement && keywordsElement.content) ||
+    "";
+
+  const keywords = joinedKeywords.split(",").flatMap((keyword) => {
+    const trimmedKeyword = keyword.trim();
+
+    return trimmedKeyword === "" ? [] : [trimmedKeyword];
+  });
+
+  return keywords.map(stringToHashTag).join(" ");
+};
+
 const getSingleImageURL = () => {
   const imageElements = document.querySelectorAll("img");
 
@@ -21,77 +194,13 @@ const getSingleImageURL = () => {
   return canvasElement.toDataURL();
 };
 
-const sendPage = () => {
-  const url = location.href;
+const getTitle = () => {
+  const ogTitleElement = document.querySelector('meta[property="og:title" i]');
 
-  const title =
-    document.querySelector('meta[property="og:title" i]')?.content ??
-    document.title;
-
-  const metaDescription =
-    document.querySelector('meta[property="og:description" i]')?.content ??
-    document.querySelector('meta[name="description" i]')?.content ??
-    "";
-
-  const metaKeywords =
-    document.querySelector('meta[name="keywords" i]')?.content ?? "";
-
-  const keywords = metaKeywords.split(",").flatMap((keyword) => {
-    const trimmedKeyword = keyword.trim();
-
-    return trimmedKeyword === "" ? [] : [trimmedKeyword];
-  });
-
-  const hashTagLine = keywords
-    .map((keyword) => `#${keyword.replaceAll(" ", "_")}`)
-    .join(" ");
-
-  const description = `${[title, url, metaDescription, hashTagLine]
-    .filter((line) => line)
-    .join("\n\n")}\n\n`;
-
-  const imageURL =
-    document.querySelector('meta[property="og:image" i]')?.content ??
-    getSingleImageURL() ??
-    document.querySelector('link[rel="icon" i]')?.href ??
-    "";
-
-  const metadata = [
-    ...[...document.querySelectorAll("meta[name]")].flatMap((metaElement) => {
-      const content = [
-        url,
-        imageURL,
-        metaDescription,
-        metaKeywords,
-        title,
-      ].reduce(
-        (previousValue, currentValue) =>
-          previousValue.replaceAll(currentValue, ""),
-        metaElement.content
-      );
-
-      return content ? [`${metaElement.name}:${content}`] : [];
-    }),
-    ...[
-      ...document.querySelectorAll('script[type="application/ld+json" i]'),
-    ].map((scriptElement) =>
-      [url, imageURL, metaDescription, metaKeywords, title].reduce(
-        (previousValue, currentValue) =>
-          previousValue.replaceAll(currentValue, ""),
-        scriptElement.innerText
-      )
-    ),
-  ].join(" ");
-
-  chrome.runtime.sendMessage({
-    url,
-    description,
-    imageURL,
-    metadata,
-    title,
-  });
+  return (
+    (ogTitleElement instanceof HTMLMetaElement && ogTitleElement.content) ||
+    document.title
+  );
 };
 
-document.addEventListener("DOMContentLoaded", sendPage);
-addEventListener("load", sendPage);
-setInterval(sendPage, 1000);
+const stringToHashTag = (string) => `#${string.replaceAll(" ", "_")}`;
